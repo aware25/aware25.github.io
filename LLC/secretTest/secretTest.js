efc88cc76a2b8afa12d638126562f70c760fc72d
aware25 <75075934+aware25@users.noreply.github.com>
2024-02-15 23:36:04 +0000

    working is so much fun!

--- LLC/secretTest/secretTest.js
+++ LLC/secretTest/secretTest.js
@@ -1,14 +1,3 @@
-// Requiring fs module in which
-// writeFile function is defined.
- 
-// Data which will write in a file.
-import data from './data.json' assert { type: 'json' };
-console.log(data);
-
-
-function abutton_that_buttons() {
-
-    console.log(data);
-
-    //testInput = document.getElementById("test").value;
+function workingisfun() {
+    console.log("working is fun!");
 }
