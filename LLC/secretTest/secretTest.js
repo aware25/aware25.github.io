0e5d407143973026fe641212baa7887e283850af
aware25 <75075934+aware25@users.noreply.github.com>
2024-02-16 14:32:58 +0000

    its getting sloppy now

--- LLC/secretTest/secretTest.js
+++ LLC/secretTest/secretTest.js
@@ -1,9 +1,20 @@
+let bbtimes;
 
 
 
 async function get_staff_times() {
     const response = await fetch("staffTimes.json");
     const times = await response.json();
-    console.log(times);
+    console.log("lil func done");
+    return times;
   }
+
+
+function get_staff_time() {
+    bbtimes = get_staff_times();
+    console.log("lil func outside butt",bbtimes);
+}
+
+bbtimes = get_staff_times();
+console.log("lil func outside",bbtimes);
   
