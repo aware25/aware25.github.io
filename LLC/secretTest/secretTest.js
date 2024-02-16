34aa9d0a28aab37ce1ac3c3a972ef909160a1dac
aware25 <75075934+aware25@users.noreply.github.com>
2024-02-16 13:19:10 +0000

    0

--- LLC/secretTest/secretTest.js
+++ LLC/secretTest/secretTest.js
@@ -4,6 +4,6 @@
 async function get_staff_times() {
     const response = await fetch("staffTimes.json");
     const times = await response.json();
-    console.log(times.last_name);
+    console.log(times[0].last_name);
   }
   
